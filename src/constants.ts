74fb6165fefd772c6bfb45327cc662feac80ec9f
tuiu <13719283454@163.com>
2025-12-23 02:02:30 +0800

    feat(types, constants): 🎉 引入新的类型定义和常量
    引入新的常量文件 `constants.ts`，定义了项目路径、文件路径模式等常量，并在 `types.ts` 中新增了 `IFileConfig`、`IPkgMap` 和 `IBundleItem` 等接口定义，以便类型检查和代码提示。

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+import { join } from "node:path";
+
+export const ROOT = join(process.cwd(), "src");
+export const PKG_MAP_PATH = join(ROOT, "pkg-map.json")
+export const BUNDLE_PATH = join(ROOT, "bundle.json")
+export const TOP_LEVEL_HIERARCHY_EXTENSIONS = [
+  ".ls",
+  ".lh"
+];
+export const PARSABLE_HIERARCHY_EXTENSIONS = [
+  ".ls",
+  ".lh",
+  ".lmat",
+  ".ltc"
+];
+export const IGNORED_HIERARCHY_SUFFIXES = [
+  ".ltcb.ls",
+  ".lanit.ls"
+]
+export const FILE_PATH_PATTERN = /^[\w./-\s-]*\.[A-Za-z0-9]{2,5}(\.[A-Za-z0-9]{2,5})?$/i;
+export const FILECONFIG_PATH = join(ROOT, "fileconfig.json")
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,22 @@ export interface Options {
   base: string;
   remote: string;
 }
+
+export interface IFileConfig {
+  [key: string]: [
+    string, string[]
+  ]
+}
+
+export interface IPkgMap {
+  [key: string]: string[];
+}
+
+export interface IBundleItem {
+  name: string;
+  zipUrl: string;
+  prefix: string;
+  selectGlob?: string;
+}
+
+export type IBundle = Array<IBundleItem>;
