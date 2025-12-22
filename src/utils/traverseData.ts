c18cf313c82244d052c32fdc2862b51b5aa7fbe7
tuiu <13719283454@163.com>
2025-12-23 02:20:14 +0800

    feat(utils): 🎉 增加数据遍历功能和路径类型定义

---
+++ src/utils/traverseData.ts
@@ -0,0 +1,65 @@
+import type { TraversePath } from "./types";
+
+export async function traverseData(
+  data: unknown,
+  callback: (
+    path: TraversePath,
+    key: string,
+    value: string
+  ) => Promise<void> | void,
+  options?: {
+    filter?: (
+      path: TraversePath,
+      key: string,
+      value: string
+    ) => boolean;
+  },
+  path: TraversePath = []
+): Promise<void> {
+  if (
+    data === null ||
+    data === undefined
+  ) {
+    return;
+  }
+
+  if (typeof data === "string") {
+    if (!options?.filter?.(path, "", data)) {
+      await callback(path, "", data);
+    }
+    return;
+  }
+
+  if (Array.isArray(data)) {
+    for (let i = 0; i < data.length; i++) {
+      await traverseData(
+        data[i],
+        callback,
+        options,
+        path.concat(i)
+      )
+    }
+    return;
+  }
+
+  if (typeof data === "object") {
+    for (const [key, value] of Object.entries(data)) {
+      const nextPath = path.concat(key);
+
+      if (options?.filter?.(nextPath, key, value)) {
+        continue;
+      }
+
+      if (typeof value === "string") {
+        await callback(nextPath, key, value);
+      } else {
+        await traverseData(
+          value,
+          callback,
+          options,
+          nextPath
+        )
+      }
+    }
+  }
+}
---
+++ src/utils/types.ts
@@ -0,0 +1,1 @@
+export type TraversePath = Array<string | number>;
