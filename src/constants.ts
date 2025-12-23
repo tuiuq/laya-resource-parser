512880def985180e53aa54959a410f0bdf164f14
tuiu <13719283454@163.com>
2025-12-23 19:33:20 +0800

    feat(constants): 🎉 Add concurrency constant
    - Introduce a new constant `CONCURRENCY` to control the level of parallelism in the build process. This allows us to fine-tune performance based on system capabilities.

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,7 @@ import { join } from "node:path";
 export const ROOT = join(process.cwd(), "src");
 export const PKG_MAP_PATH = join(ROOT, "pkg-map.json")
 export const BUNDLE_PATH = join(ROOT, "bundle.json")
+export const CONCURRENCY = 5;
 export const TOP_LEVEL_HIERARCHY_EXTENSIONS = [
   ".ls",
   ".lh"
